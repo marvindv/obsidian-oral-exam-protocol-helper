8a2f7c3c743785abbfe7373a5799ab1e27e55aa3
Marvin Davieds <marvin.davieds@gmail.com>
2023-07-23 22:13:10 +0200

    add existing code

--- README.md
+++ README.md
@@ -1,50 +1,15 @@
-# Obsidian Sample Plugin
+# Obsidian Oral Exam Protocol Helper
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This is a simple plugin that helps to count points and calculate a grade for protocols of the following form:
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+```markdown
+- This is the first question? This is a partial answer that is right +, a partial answer that is wrong -, and now the last partial answer that is somewhat right +-
+- What is the meaning of this and that? This is the right answer +
+```
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+Which translates to a maximum number of 2 points (1 for each line of questions) and 1.5 actually archived points (2/4 points for the first line and 1/1 point for the second line). The points per line is defined by the fraction of `+` signs in relation to `+` and `-` signs.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
-
-## First time developing plugins?
-
-Quick starting guide for new plugin devs:
-
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
-
-## Releasing new releases
-
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
-
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
-
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+The grade is calculated based on the percentage of archived points, ranging from at least 60% for 4.0 to at least 94% for grade 1.0. This is very much specific to the regulations of the educational institution the author of this plugin is employed at.
 
 ## How to use
 
@@ -65,31 +30,3 @@ Quick starting guide for new plugin devs:
   - eslint will then create a report with suggestions for code improvement by file and line number.
 - If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
   - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
--- main.ts
+++ main.ts
@@ -1,134 +1,120 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Notice, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+/**
+ * Calculates from a number between 0..1 representing the percentage of right answers the resulting grade.
+ * Based on https://www.ief.uni-rostock.de/storages/uni-rostock/Alle_IEF/IEF/Ordnungen/RPO/RPO_BSc_MSc_2023_-_UNI_HRO_-_Amtliche_Bekanntmachungen_-_NR_5_2023.pdf
+ * page 17.
+ */
+function percentToGrade(percent: number): number {
+	const minPercent = 0.6;
+	const aboveMin = 1 - minPercent;
 
-interface MyPluginSettings {
-	mySetting: string;
+	// Maps each grade to the required percentage of points archived above the minimum.
+	const gradeMap = [
+		// E.g., for 1.0 one has to archive at least 85% of the points above the minimum score.
+		[1.0, 0.85],
+		[1.3, 0.75],
+		[1.7, 0.67],
+		[2.0, 0.59],
+		[2.3, 0.5],
+		[2.7, 0.42],
+		[3.0, 0.34],
+		[3.3, 0.25],
+		[3.7, 0.12],
+		[4.0, 0],
+	];
+
+	for (const [grade, percentAboveMin] of gradeMap) {
+		if (percent >= minPercent + percentAboveMin * aboveMin) {
+			return grade;
+		}
+	}
+
+	return 5.0;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class OralExamProtocolHelperPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		const plusMinuxRegex = /[\+-]/g;
+		const ulLineStart = /^[-*+] (.*)/;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "calculate-points-and-grade",
+			name: "Calculate points and a grade",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				const noteFile = this.app.workspace.getActiveFile();
+				if (!noteFile) {
+					new Notice("Please open a oral exam protocol.");
+					return;
+				}
+
+				// Iterate over each line, skipping frontmatter, count + and -
+				// for each unordered list item. The points for each line is
+				// the proportion of + signs against - signs in that line.
+				// Therefore per line the point has a range of [0, 1].
+
+				let maxPoints = 0;
+				let points = 0;
+				let inFrontmatter = false;
+				for (let i = 0; i < editor.lineCount(); i++) {
+					const line = editor.getLine(i).trim();
+					// Skip empty lines.
+					if (line === "") {
+						continue;
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					// Handle frontmatter start and end. This is very
+					// rudementary but for this plugin just assume there is no
+					// invalid second frontmatter block. If it is, its content
+					// will just as well be ignored.
+					if (line === "---") {
+						inFrontmatter = !inFrontmatter;
+						continue;
+					}
+
+					// Ignore frontmatter.
+					if (inFrontmatter) {
+						continue;
+					}
+
+					const lineMatch = line.match(ulLineStart);
+					if (!lineMatch) {
+						// Line is not an unordered list item.
+						continue;
+					}
+
+					const lineContent = lineMatch[1];
+
+					const match = lineContent.match(plusMinuxRegex);
+					if (!match) {
+						// Lines does not contain + or - so ignore the line.
+						continue;
+					}
+
+					maxPoints += 1;
+					const linePoints = match.reduce(
+						(prev, curr) =>
+							curr === "+" ? prev + 1 / match.length : prev,
+						0
+					);
+					points += linePoints;
 				}
-			}
+
+				// Update the documents frontmatter with the calculated values.
+				this.app.fileManager.processFrontMatter(
+					noteFile,
+					(frontmatter) => {
+						frontmatter.punkte = `${points.toFixed(
+							2
+						)}/${maxPoints}`;
+						frontmatter.vorgeschlageneNote = percentToGrade(
+							points / maxPoints
+						);
+					}
+				);
+			},
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	onunload() {}
 }
